bf09c1c0d2e1db165c8021187c78700a63c71a6b
PhilippFors <57153492+PhilippFors@users.noreply.github.com>
2019-11-13 21:31:22 +0100

    Growing

---
+++ Assets/Growing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Growing : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Camera camera;
+    [SerializeField] private string selectable = "Selectable";
+    private CharacterController charcontrol;
+    void Start()
+    {
+        charcontrol = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            var ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var selection = hit.transform;
+                if (selection.CompareTag(selectable))
+                {
+                    charcontrol.radius = 0.3f;
+                    charcontrol.height = 1.5f;
+                }
+            }
+        }
+    }
+}
