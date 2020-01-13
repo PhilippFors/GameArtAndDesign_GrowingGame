2b1a8eb2bfa8da416a683e523c20bcb36e669067
Philipp Forstner <S1810238022@students.fh-hagenberg.at>
2020-01-13 15:49:59 +0100

    Singleton setup, delete unnecessary files

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,20 +6,29 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private CharacterController charcontrol;
-    private States state;
     Vector3 vec = new Vector3(0, 2, 0);
-    void Start()
-    {
-        state = gameObject.GetComponent<States>();
-    }
 
+
+    //Singleton Setup
+    //Call Methods or Variables with GameManager.Instance.Method()/Variable
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("Null");
+
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
     public void Growing()
     {
-        if (state.first)
-        {
-            charcontrol.transform.Translate(vec);
-            charcontrol.height = 1.6f;
-            state.first = false;
-        }
+        charcontrol.transform.Translate(vec);
+        charcontrol.height = 1.6f;
     }
 }
