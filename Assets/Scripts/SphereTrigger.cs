2560a40870303a1691563fa34807152fa683b26f
PhilippFors <S1810238022@students.fh-hagenberg.at>
2020-01-14 14:13:30 +0100

    Toybox and bed

---
+++ Assets/Scripts/SphereTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereTrigger : MonoBehaviour
+{
+    public bool triggered = false;
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name.Equals("Sphere"))
+        {
+            triggered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        triggered = false;
+    }
+}
